cb79ee34287c1a6cf63e80661625077a4257bc37
Eliot Dresselhaus <edressel@platinasystems.com>
2016-08-04 11:58:45 +0200

    tuntap create interfaces with admin up + link state reflected in iff_up & iff_running
    Signed-off-by: Eliot Dresselhaus <edressel@platinasystems.com>

--- unix/tuntap/tuntap.go
+++ unix/tuntap/tuntap.go
@@ -3,10 +3,13 @@
 package tuntap
 
 import (
+	"github.com/platinasystems/elib"
 	"github.com/platinasystems/elib/parse"
 	"github.com/platinasystems/vnet"
+	"github.com/platinasystems/vnet/ethernet"
 
 	"fmt"
+	"strings"
 	"syscall"
 	"unsafe"
 )
@@ -32,8 +35,20 @@ func readv(fd int, iov []syscall.Iovec) (int, error)  { return rwv(fd, iov, fals
 func writev(fd int, iov []syscall.Iovec) (int, error) { return rwv(fd, iov, true) }
 
 type Interface struct {
-	hi vnet.Hi
-	si vnet.Si
+	hi        vnet.Hi
+	si        vnet.Si
+	name      ifreq_name
+	poolIndex uint // index in ifPool
+	socket    int  // provisioning socket
+	ifindex   int  // linux interface index
+	flags     iff_flag
+}
+
+//go:generate gentemplate -d Package=tuntap -id ifPool -d PoolType=ifPool -d Type=Interface -d Data=elts github.com/platinasystems/elib/pool.tmpl
+
+func (m *Main) interfaceForSi(si vnet.Si) (i *Interface) {
+	i = &m.ifPool.elts[m.ifPoolIndexBySi[si]]
+	return
 }
 
 type Main struct {
@@ -42,7 +57,7 @@ type Main struct {
 	v *vnet.Vnet
 
 	// Selects whether we create tun or tap interfaces.
-	isTap bool
+	isTun bool
 
 	disable parse.Enable
 
@@ -50,6 +65,11 @@ type Main struct {
 	dev_net_tun_fd int
 
 	mtu_bytes, mtu_buffers uint32
+
+	ifPool ifPool
+
+	ifPoolIndexByName map[ifreq_name]uint
+	ifPoolIndexBySi   elib.Uint32Vec
 }
 
 func Init(v *vnet.Vnet) {
@@ -68,22 +88,248 @@ const (
 	iff_nofilter = 1 << 10
 )
 
-type ifReq struct {
-	name  [16]byte
-	flags uint16
-	_     [0x28 - 16 - 2]byte
+type ifreq_name [16]byte
+
+func (n ifreq_name) String() string { return strings.TrimRight(string(n[:]), "\x00") }
+
+// Linux interface flags
+type iff_flag int
+
+const (
+	iff_up_bit, iff_up iff_flag = iota, 1 << iota
+	iff_broadcast_bit, iff_broadcast
+	iff_debug_bit, iff_debug
+	iff_loopback_bit, iff_loopback
+	iff_pointopoint_bit, iff_pointopoint
+	iff_notrailers_bit, iff_notrailers
+	iff_running_bit, iff_running
+	iff_noarp_bit, iff_noarp
+	iff_promisc_bit, iff_promisc
+	iff_allmulti_bit, iff_allmulti
+	iff_master_bit, iff_master
+	iff_slave_bit, iff_slave
+	iff_multicast_bit, iff_multicast
+	iff_portsel_bit, iff_portsel
+	iff_automedia_bit, iff_automedia
+	iff_dynamic_bit, iff_dynamic
+	iff_lower_up_bit, iff_lower_up
+	iff_dormant_bit, iff_dormant
+	iff_echo_bit, iff_echo
+)
+
+var iff_flag_names = [...]string{
+	iff_up_bit:          "up",
+	iff_broadcast_bit:   "broadcast",
+	iff_debug_bit:       "debug",
+	iff_loopback_bit:    "loopback",
+	iff_pointopoint_bit: "point-to-point",
+	iff_notrailers_bit:  "no-trailers",
+	iff_running_bit:     "running",
+	iff_noarp_bit:       "no-arp",
+	iff_promisc_bit:     "promiscuous",
+	iff_allmulti_bit:    "all-multicast",
+	iff_master_bit:      "master",
+	iff_slave_bit:       "slave",
+	iff_multicast_bit:   "multicast",
+	iff_portsel_bit:     "portsel",
+	iff_automedia_bit:   "automedia",
+	iff_dynamic_bit:     "dynamic",
+	iff_lower_up_bit:    "lower-up",
+	iff_dormant_bit:     "dormant",
+	iff_echo_bit:        "echo",
 }
 
-func (m *Main) setupInterface(hw *vnet.HwIf) {
+func (f iff_flag) String() string { return elib.FlagStringer(iff_flag_names[:], elib.Word(f)) }
+
+type ifreq_flags struct {
+	name  ifreq_name
+	flags uint16
+}
+
+type ifreq_int struct {
+	name ifreq_name
+	i    int
+}
+
+type ifreq_sockaddr_any struct {
+	name     ifreq_name
+	sockaddr syscall.RawSockaddrAny
+}
+
+type ifreq_type int
+
+const (
+	ifreq_TUNSETIFF     ifreq_type = syscall.TUNSETIFF
+	ifreq_TUNSETPERSIST ifreq_type = syscall.TUNSETPERSIST
+	ifreq_GETIFINDEX    ifreq_type = syscall.SIOCGIFINDEX
+	ifreq_GETIFFLAGS    ifreq_type = syscall.SIOCGIFFLAGS
+	ifreq_SETIFFLAGS    ifreq_type = syscall.SIOCSIFFLAGS
+	ifreq_SETIFHWADDR   ifreq_type = syscall.SIOCSIFHWADDR
+)
+
+var ifreq_type_names = map[ifreq_type]string{
+	ifreq_TUNSETIFF:     "TUNSETIFF",
+	ifreq_TUNSETPERSIST: "TUNSETPERSIST",
+	ifreq_GETIFINDEX:    "GETIFINDEX",
+	ifreq_GETIFFLAGS:    "GETIFFLAGS",
+	ifreq_SETIFFLAGS:    "SETIFFLAGS",
+	ifreq_SETIFHWADDR:   "SETIFHWADDR",
+}
+
+func (t ifreq_type) String() string {
+	if s, ok := ifreq_type_names[t]; ok {
+		return s
+	}
+	return fmt.Sprintf("0x%x", t)
 }
 
 // Create tuntap interfaces for all vnet interfaces not marked as special.
 func (m *Main) okHi(hi vnet.Hi) (ok bool) { return m.v.HwIfer(hi).IsUnix() }
 func (m *Main) okSi(si vnet.Si) bool      { return m.okHi(m.v.SupHi(si)) }
 
+func (m *Main) ioctl(req ifreq_type, arg uintptr) (err error) {
+	_, _, e := syscall.Syscall(syscall.SYS_IOCTL, uintptr(m.dev_net_tun_fd), uintptr(req), arg)
+	if e != 0 {
+		err = fmt.Errorf("tuntap ioctl %s: %s", req, e)
+	}
+	return
+}
+
+func (i *Interface) ioctl(req ifreq_type, arg uintptr) (err error) {
+	_, _, e := syscall.Syscall(syscall.SYS_IOCTL, uintptr(i.socket), uintptr(req), arg)
+	if e != 0 {
+		err = fmt.Errorf("tuntap %s ioctl %s: %s", i.name, req, e)
+	}
+	return
+}
+
 func (m *Main) SwIfAddDel(v *vnet.Vnet, si vnet.Si, isDel bool) (err error) {
-	if !m.okSi(si) {
+	hi := m.v.SupHi(si)
+	if !m.okHi(hi) {
 		return
+	}
+
+	// Tuntap interfaces are never deleted; only created.
+	if isDel {
+		return
+	}
+
+	intf := Interface{
+		hi: hi,
+		si: si,
+	}
+
+	copy(intf.name[:], si.Name(v))
+
+	// Create interface (set flags) and make persistent (e.g. interface stays around when we die).
+	{
+		r := ifreq_flags{name: intf.name}
+		r.flags = iff_no_pi
+		if m.isTun {
+			r.flags |= iff_tun
+		} else {
+			r.flags |= iff_tap
+		}
+		if err = m.ioctl(ifreq_TUNSETIFF, uintptr(unsafe.Pointer(&r))); err != nil {
+			return
+		}
+		if err = m.ioctl(ifreq_TUNSETPERSIST, 1); err != nil {
+			return
+		}
+	}
+
+	// Create provisioning socket.
+	eth_p_all := uint16(vnet.Uint16(syscall.ETH_P_ALL).FromHost())
+	if intf.socket, err = syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, int(eth_p_all)); err != nil {
+		err = fmt.Errorf("tuntap socket: %s", err)
+		return
+	}
+	defer func() {
+		if err != nil {
+			syscall.Close(intf.socket)
+		}
+	}()
+
+	if err = syscall.SetNonblock(intf.socket, true); err != nil {
+		err = fmt.Errorf("tuntap set non-blocking: %s", err)
+		return
+	}
+
+	// Find linux interface index.
+	{
+		r := ifreq_int{name: intf.name}
+		if err = intf.ioctl(ifreq_GETIFINDEX, uintptr(unsafe.Pointer(&r))); err != nil {
+			return
+		}
+		intf.ifindex = r.i
+	}
+
+	// Bind the provisioning socket to the interface.
+	{
+		sa := syscall.SockaddrLinklayer{
+			Ifindex:  intf.ifindex,
+			Protocol: eth_p_all,
+		}
+		if err = syscall.Bind(intf.socket, &sa); err != nil {
+			err = fmt.Errorf("tuntap bind: %s", err)
+			return
+		}
+	}
+
+	// Fetch initial interface flags.
+	{
+		r := ifreq_int{name: intf.name}
+		if err = intf.ioctl(ifreq_GETIFFLAGS, uintptr(unsafe.Pointer(&r))); err != nil {
+			return
+		}
+		intf.flags = iff_flag(r.i)
+	}
+
+	if eifer, ok := m.v.HwIfer(hi).(ethernet.HwInterfacer); ok {
+		ei := eifer.GetInterface()
+
+		// For tap interfaces, set ethernet address of interface.
+		if !m.isTun {
+			r := ifreq_sockaddr_any{name: intf.name}
+			r.sockaddr.Addr.Family = syscall.ARPHRD_ETHER
+			for i := range ei.Address {
+				r.sockaddr.Addr.Data[i] = int8(ei.Address[i])
+			}
+			if err = intf.ioctl(ifreq_SETIFHWADDR, uintptr(unsafe.Pointer(&r))); err != nil {
+				return
+			}
+		}
+	}
+
+	intf.poolIndex = m.ifPool.GetIndex()
+	m.ifPool.elts[intf.poolIndex] = intf
+
+	m.ifPoolIndexBySi.ValidateInit(uint(si), ^uint32(0))
+	m.ifPoolIndexBySi[si] = uint32(intf.poolIndex)
+	if m.ifPoolIndexByName == nil {
+		m.ifPoolIndexByName = make(map[ifreq_name]uint)
+	}
+	m.ifPoolIndexByName[intf.name] = intf.poolIndex
+	return
+}
+
+func (m *Main) maybeChangeFlag(si vnet.Si, isUp bool, flag iff_flag) (err error) {
+	intf := m.interfaceForSi(si)
+	change := false
+	switch {
+	case isUp && intf.flags&flag == 0:
+		change = true
+		intf.flags |= flag
+	case !isUp && intf.flags&flag != 0:
+		change = true
+		intf.flags &^= flag
+	}
+	if change {
+		r := ifreq_int{
+			name: intf.name,
+			i:    int(intf.flags),
+		}
+		err = intf.ioctl(ifreq_SETIFFLAGS, uintptr(unsafe.Pointer(&r)))
 	}
 	return
 }
@@ -92,6 +338,7 @@ func (m *Main) SwIfAdminUpDown(v *vnet.Vnet, si vnet.Si, isUp bool) (err error) 
 	if !m.okSi(si) {
 		return
 	}
+	err = m.maybeChangeFlag(si, isUp, iff_up)
 	return
 }
 
@@ -99,6 +346,7 @@ func (m *Main) HwIfLinkUpDown(v *vnet.Vnet, hi vnet.Hi, isUp bool) (err error) {
 	if !m.okHi(hi) {
 		return
 	}
+	err = m.maybeChangeFlag(v.HwIf(hi).Si(), isUp, iff_running)
 	return
 }
 
@@ -124,6 +372,21 @@ func (m *Main) Init() (err error) {
 	return
 }
 
+// Shutdown interfaces on main loop exit.
+func (m *Main) Exit() (err error) {
+	m.ifPool.Foreach(func(intf Interface) {
+		intf.flags &^= iff_up | iff_running
+		r := ifreq_int{
+			name: intf.name,
+			i:    int(intf.flags),
+		}
+		intf.ioctl(ifreq_SETIFFLAGS, uintptr(unsafe.Pointer(&r)))
+		syscall.Close(intf.socket)
+	})
+	syscall.Close(m.dev_net_tun_fd)
+	return
+}
+
 func (m *Main) Configure(in *parse.Input) {
 	// Suitable defaults for an Ethernet-like tun/tap device.
 	if m.mtu_bytes == 0 {
@@ -134,9 +397,9 @@ func (m *Main) Configure(in *parse.Input) {
 		switch {
 		case in.Parse("mtu %d", &m.mtu_bytes):
 		case in.Parse("tap"):
-			m.isTap = true
+			m.isTun = false
 		case in.Parse("tun"):
-			m.isTap = false
+			m.isTun = true
 		case in.Parse("en*able"):
 			m.disable = false
 		case in.Parse("dis*able"):
