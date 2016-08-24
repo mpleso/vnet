6159d0694ee4970d3b0a6b119999b6535a406c9e
Eliot Dresselhaus <edressel@platinasystems.com>
2016-08-24 14:10:24 +0200

    rename pci -> pci-discovery
    Signed-off-by: Eliot Dresselhaus <edressel@platinasystems.com>

--- devices/bus/pci/pci.go
+++ devices/bus/pci/pci.go
@@ -9,7 +9,8 @@ type pciDiscover struct{ vnet.Package }
 
 func (d *pciDiscover) Init() error { return pci.DiscoverDevices() }
 func Init(v *vnet.Vnet) {
-	if _, ok := v.PackageByName("pci"); !ok {
-		v.AddPackage("pci", &pciDiscover{})
+	name := "pci-discovery"
+	if _, ok := v.PackageByName(name); !ok {
+		v.AddPackage(name, &pciDiscover{})
 	}
 }
