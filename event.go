3a7c3df79be0ff50b1dc67801fb4a98b0946277d
Eliot Dresselhaus <edressel@platinasystems.com>
2016-09-08 15:45:24 +0200

    timed events
    Signed-off-by: Eliot Dresselhaus <edressel@platinasystems.com>

--- event.go
+++ event.go
@@ -33,3 +33,13 @@ func (v *Vnet) SignalEvent(r Eventer) {
 	l := v.GetLoop()
 	l.AddEvent(r, &v.eventMain.eventNode)
 }
+
+func (v *Vnet) AddTimedEvent(r Eventer, dt float64) {
+	e := r.GetEvent()
+	e.v = v
+	l := v.GetLoop()
+	l.AddTimedEvent(r, &v.eventMain.eventNode, dt)
+}
+
+func (e *Event) SignalEvent(r Eventer)               { e.v.SignalEvent(r) }
+func (e *Event) AddTimedEvent(r Eventer, dt float64) { e.v.AddTimedEvent(r, dt) }
